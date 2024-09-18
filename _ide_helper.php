1947c30274ab180419ab20701920dc93927ac3bb
Murat KÖSE <muratkose1126@gmail.com>
2024-09-18 13:45:19 +0300

    güncelleme

--- _ide_helper.php
+++ _ide_helper.php
@@ -19049,6 +19049,26 @@ namespace Illuminate\Routing {
         {
                         return \Illuminate\Routing\Route::lazy($enabled);
         }
+                    /**
+         * 
+         *
+         * @see \Spatie\Permission\PermissionServiceProvider::registerMacroHelpers()
+         * @param mixed $roles
+         * @static 
+         */        public static function role($roles = [])
+        {
+                        return \Illuminate\Routing\Route::role($roles);
+        }
+                    /**
+         * 
+         *
+         * @see \Spatie\Permission\PermissionServiceProvider::registerMacroHelpers()
+         * @param mixed $permissions
+         * @static 
+         */        public static function permission($permissions = [])
+        {
+                        return \Illuminate\Routing\Route::permission($permissions);
+        }
             }
     }
 
@@ -25677,6 +25697,11 @@ namespace  {
     }
 
 
+namespace Facades\Livewire\Features\SupportFileUploads {
+    /**
+     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
+    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
+}
 
 
 
